e765c1de89ce3c243596182fe0f7a95978c3f68c
Ravi98351 <raviapk983@gmail.com>
2022-07-23 13:39:57 +0530

    adding day 4 task

---
+++ Ravi_Roshan_fw18_0579/component/sidebar.js
@@ -0,0 +1,407 @@
+function container(){
+    return `
+
+<div id="container11">
+            
+<div class="container111">
+    <div class="head">Brand</div>
+    <hr>
+    <div class="checkbox">
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox" value="all">
+                <div class="p">All</div>
+            </div>
+
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox" value="alva">
+                <div class="p">Alva</div>
+            </div>
+
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox" value="almay">
+                <div class="p">Almay</div>
+            </div>
+
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox" value="anna sui">
+                <div class="p">Anna sui</div>
+            </div>
+
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox" value="annabelle">
+                <div class="p">Annabelle</div>
+            </div>
+
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox" value="benefit">
+                <div class="p">Benefit</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="boosh">
+            <div class="p">Boosh</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="burt's bees">
+            <div class="p">Burt's bees</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="butter london">
+            <div class="p">Butter london</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="c'est moi">
+            <div class="p">C'est moi</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="cargo cosmetics">
+            <div class="p">Cargo cosmetics</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="china glaze">
+            <div class="p">China glaze</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="clinique">
+            <div class="p">Clinique</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="coastal classic creation">
+            <div class="p">Coastal classic creation</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="colourpop">
+            <div class="p">Colourpop</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="covergirl">
+            <div class="p">Covergirl</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="dalish">
+            <div class="p">Dalish</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="deciem">
+            <div class="p">Deciem</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="dior">
+            <div class="p">Dior</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="dr. hauschka">
+            <div class="p">Dr. hauschka</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="e.l.f.">
+            <div class="p">E.l.f.</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="essie">
+            <div class="p">Essie</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="fenty">
+            <div class="p">Fenty</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="glossier">
+            <div class="p">Glossier</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="green people">
+            <div class="p">Green people</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="iman">
+            <div class="p">Iman</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="">
+            <div class="p"></div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="l'oreal">
+            <div class="p">L'oreal</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="lotus cosmetics usa">
+            <div class="p">Lotus cosmetics usa</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="maia's mineral galaxy">
+            <div class="p">Maia's mineral galaxy</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="marcelle">
+            <div class="p">Marcelle</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="marienatie">
+            <div class="p">Marienatie</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="maybelline">
+            <div class="p">Maybelline</div>
+            </div>
+            
+            
+    </div>
+</div>
+
+
+<div class="container111">
+    <div class="head">Types</div>
+    <hr>
+    <div class="checkbox">
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox" value="blush">
+                <div class="p">Blush</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="Bronzer">
+            <div class="p">Bronzer</div>
+        </div>
+
+        <div class="checkbox1">
+        <input type="checkbox" class="smallcheckbox" value="Eyebrow">
+        <div class="p">Eyebrow</div>
+    </div>
+
+    <div class="checkbox1">
+    <input type="checkbox" class="smallcheckbox" value="Eyeliner">
+    <div class="p">Eyeliner</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Eyeshadow">
+<div class="p">Eyeshadow</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Foundation">
+<div class="p">Foundation</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Lip liner">
+<div class="p">Lip liner</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Lipstick">
+<div class="p">Lipstick</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Mascara">
+<div class="p">Mascara</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Nail polish">
+<div class="p">Nail polish</div>
+</div>
+
+
+            
+    </div>
+</div>
+
+
+<div class="container111">
+    <div class="head">Tag</div>
+    <hr>
+    <div class="checkbox">
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox" value="Vegan">
+                <div class="p">Vegan</div>
+            </div>
+
+            <div class="checkbox1">
+            <input type="checkbox" class="smallcheckbox" value="Canadian">
+            <div class="p">Canadian</div>
+        </div>
+
+        <div class="checkbox1">
+        <input type="checkbox" class="smallcheckbox" value="Natural">
+        <div class="p">Natural</div>
+    </div>
+
+    <div class="checkbox1">
+    <input type="checkbox" class="smallcheckbox" value="Gluten free ">
+    <div class="p">Gluten free </div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Fair trade">
+<div class="p">Fair trade</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Sugar free">
+<div class="p">Sugar free</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Non-gmo">
+<div class="p">Non-gmo</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Dairy free">
+<div class="p">Dairy free</div>
+</div>
+
+
+         
+    </div>
+</div>
+
+
+<div class="container111">
+    <div class="head">Category</div>
+    <hr>
+    <div class="checkbox">
+    <div class="checkbox1">
+    <input type="checkbox" class="smallcheckbox" value="Concealer">
+    <div class="p">Concealer</div>
+    </div>
+
+    <div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Pencil ">
+<div class="p">Pencil</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Highlighter">
+<div class="p">Highlighter</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Powder">
+<div class="p">Powder</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Mineral">
+<div class="p">Mineral</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Cream">
+<div class="p">Cream</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Bb cc ">
+<div class="p">Bb cc </div>
+</div>
+
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Contour">
+<div class="p">Contour</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Liquid">
+<div class="p">Liquid</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Concealer">
+<div class="p">Concealer</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Palette">
+<div class="p">Palette</div>
+</div>
+
+<div class="checkbox1">
+<input type="checkbox" class="smallcheckbox" value="Powder">
+<div class="p">Powder</div>
+</div>
+    </div>
+</div>
+
+
+
+
+
+
+<div class="container111">
+    <div class="head">Skin Type</div>
+    <hr>
+    <div class="checkbox">
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox">
+                <div class="p">Combination</div>
+            </div>
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox">
+                <div class="p">Dry</div>
+            </div>
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox">
+                <div class="p">Mature</div>
+            </div>
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox">
+                <div class="p">Normal</div>
+            </div>
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox">
+                <div class="p">Oily</div>
+            </div>
+            <div class="checkbox1">
+                <input type="checkbox" class="smallcheckbox">
+                <div class="p">Sensitive</div>
+            </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+</div>
+<div id="container12"></div>
+`
+
+}
+
+export {container}
