c1f7886be1b885aed56e6f5820766ddd6bcc0e7b
salonidhatrak <saloni123@gmail.com>
2022-07-23 21:56:05 +0530

    final changes

---
+++ saloni_fw17_0338/components/footer.js
@@ -0,0 +1,155 @@
+
+let footer=()=>{
+    return `<div class="footer1">
+    <p>Website feedback? Let us know ></p>
+</div>
+<div class="footer2">
+
+    <div class="labelBox">
+        <div class="label-text">
+
+
+            <div>
+                <span><i class="fa-solid fa-shop"></i> Find a store</span>
+                <p><i class="fa-solid fa-shop-lock"></i> Choose Your Store</p>
+            </div>
+        </div>
+        <div class="label-text">
+            <div>
+                <span><i class="fa-solid fa-hand-holding-hand"></i> Live Beauty Help</span>
+                <p><i class="fa-solid fa-comment-slash"></i> Chat is unavailable</p>
+            </div>
+        </div>
+        <div class="label-text">
+
+            <div>
+                <span>
+                    <i class="fa-solid fa-phone"></i> 1-877-737-4672
+                </span>
+                <p><i class="fa-solid fa-phone-office"></i> TTY: 1-888-866-9845 </p>
+            </div>
+        </div>
+        <div class="label-text">
+
+            <div>
+                <span><i class="fa-solid fa-mobile-screen-button"></i> Get the App</span>
+                <p><i class="fa-solid fa-comment-sms"></i> Text "APP" to 36681</p>
+            </div>
+        </div>
+        <div class="label-text">
+
+            <div>
+                <span><i class="fa-solid fa-credit-card-front"></i> Skin Store Credit Card Program
+
+                </span>
+                <p><i class="fa-brands fa-discourse"></i> Want 25% off your Skin Store purchase1? DETAILS</p>
+            </div>
+        </div>
+    </div>
+    <hr />
+    <div class="details">
+        <div>
+            <h6>About Skin Store</h6>
+            <p>About Skin Store</p>
+            <p>Key Workers Discount</p>
+            <p>About Us</p>
+            <p> Affiliate Program</p>
+            <p> Brand Directory</p>
+            <p> Coupon Codes</p>
+            <p> Refer A Friend</p>
+            <p> Gift Cards</p>
+            <p> Student Discount</p>
+            <p> Join SkinStore Experts</p>
+            <p> Checkout on Instagram</p>
+
+
+        </div>
+
+        <div>
+            <h6>My Skin Store</h6>
+            <p> Beauty Insider</p>
+            <p> Skin Store Credit Card</p>
+            <p> Community Profile</p>
+            <p> Order Status</p>
+            <p> Purchase History</p>
+            <p> Account Settings</p>
+            <p> Beauty Advisor Recommendations</p>
+            <p> Beauty Offers</p>
+            <p> Quizzes & Buying Guides</p>
+            <p> Rewards Bazaar</p>
+            <p> Loves</p>
+            <p> Book a Reservation</p>
+        </div>
+
+        <div>
+            <h6>Help</h6>
+            <p>
+                Customer Service</p>
+            <p> Returns & Exchanges</p>
+            <p> Delivery and Pickup Options</p>
+            <p> Shipping</p>
+            <p> Billing</p>
+            <p> International Shipments</p>
+            <p> Beauty Services FAQ</p>
+            <p> Skin Store at Kohl's</p>
+            <p> Skin Store Inside JCPenney</p>
+            <p> Store Locations</p>
+            <p> Online Ordering</p>
+            <p> Klarna</p>
+            <p> Accessibility</p>
+
+
+        </div>
+
+
+        <div>
+            <h6>Region & Language</h6>
+            <p><img class="countries" src="https://www.sephora.com/img/ufe/flags/us.svg" /> United States - English
+            </p>
+            <p><img class="countries" src="https://www.sephora.com/img/ufe/flags/ca.svg" /> Cannada - English</p>
+            <p><img class="countries" src="https://www.sephora.com/img/ufe/flags/ca.svg" /> Cannada - Francais</p>
+
+
+        </div>
+        <div class="last">
+            <h2>We Belong to
+                Something Beautiful</h2>
+            <div class="labelInput2">
+
+                <div class="labelInput">
+
+                    <label for="sign">Sign up for Skin Store Emails</label>
+                    <input type="text" placeholder="Enter your email address" />
+                </div>
+                <button id="labelInput_btn">Sign up</button>
+            </div>
+
+        </div>
+
+    </div>
+    <hr />
+    <div class="last2">
+        <div>
+            <p>© 2022 Skin Store USA, Inc. All rights reserved.</p>
+            <span>Privacy Policy</span> <span>Terms of Use Accessibility</span>
+            <span>Sitemap</span> <span> CA - Do Not Sell My Personal Information</span>
+        </div>
+
+        <div id="icons">
+            <img src="https://www.sephora.com/img/ufe/icons/instagram-ko.svg" />
+            <img src="https://www.sephora.com/img/ufe/icons/facebook-ko.svg" />
+            <img src="https://www.sephora.com/img/ufe/icons/twitter-ko.svg" />
+            <img src="https://www.sephora.com/img/ufe/icons/youtube-ko.svg" />
+            <img src="https://www.sephora.com/img/ufe/icons/pinterest-ko.svg" />
+            <img src="https://www.sephora.com/img/ufe/icons/snapchat-ko.svg" />
+
+            <img src="https://www.sephora.com/img/ufe/icons/tiktok-ko.svg" />
+
+        </div>
+    </div>
+
+</div>`
+};
+
+
+export default footer;
